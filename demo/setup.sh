a4882efb8c9d15c5dba0141ca59e204f2d92a7e0
Tom Twyman <thomas.twyman@docker.com>
2025-04-30 13:12:00 -0400

    updated order of commands in clean.sh
    Signed-off-by: Tom Twyman <thomas.twyman@docker.com>

--- demo/setup.sh
+++ demo/setup.sh
@@ -17,8 +17,9 @@ git pull
 git checkout -b $BRANCH_NAME
 
 echo "==> Applying patch and creating a commit"
-npm install express@4.17.1 --save
 git apply --whitespace=fix "${SCRIPT_DIR}/dockerfile.patch"
+npm cache clean --force
+npm install express@4.17.1 --save
 git commit -sam "reset repo to introduce vulns for demo"
 
 echo "==> Configure / ensure use of local builder"
