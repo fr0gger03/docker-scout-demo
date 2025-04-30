0fb6d16f9af6d494fb7be671ad9c41158fda1104
Tom Twyman <thomas.twyman@docker.com>
2025-04-30 12:20:03 -0400

    added script and patch for clean repo
    Signed-off-by: Tom Twyman <thomas.twyman@docker.com>

---
+++ demo/clean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo "==> Commit and clean"
+git commit -sam "commit before clean"
+git clean -f
+
+echo "==> Delete the current branch, and reset to upstream main"
+
+git checkout temp
+git branch -D main
+git checkout origin/main
+git checkout -b main
+git push -u origin main --force
+
+
+# echo "==> Applying patch and creating a commit"
+# git apply --whitespace=fix "${SCRIPT_DIR}/clean.patch"
+# git commit -am "reset repo to remove vulns from repo"
